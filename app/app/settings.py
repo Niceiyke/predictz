693c9962fe117e4398697480244074c4dc402ca9
Niceiyke <oyomworld@gmail.com>
2022-09-12 22:07:32 +0100

    m

--- app/app/settings.py
+++ app/app/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT ='/static'
 
 # Redis and Celery Conf
 CELERY_BROKER_URL = "redis://redis:6379"
